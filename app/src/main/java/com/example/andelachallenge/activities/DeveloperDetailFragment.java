a9532b14ac87a8ab2b356040b169687a2614a33e
Fapohunda Kanyinsola Oyindamola <lilkaydeee@gmail.com>
2017-03-07 07:35:25 +0100

    Added functionality to the profile screen fragment

--- app/src/main/java/com/example/andelachallenge/activities/DeveloperDetailFragment.java
+++ app/src/main/java/com/example/andelachallenge/activities/DeveloperDetailFragment.java
@@ -1,16 +1,21 @@
 package com.example.andelachallenge.activities;
 
 import android.app.Activity;
-import android.support.design.widget.CollapsingToolbarLayout;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.example.andelachallenge.R;
-import com.example.andelachallenge.activities.dummy.DummyContent;
+import com.example.andelachallenge.model.Developer;
 
 /**
  * A fragment representing a single Item detail screen.
@@ -23,12 +28,14 @@ public class DeveloperDetailFragment extends Fragment {
      * The fragment argument representing the item ID that this fragment
      * represents.
      */
+    public ImageView mImageView;
+
     public static final String ARG_ITEM = "item_id";
 
     /**
      * The dummy content this fragment is presenting.
      */
-    private DummyContent.DummyItem mItem;
+    private Developer mDeveloper;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -37,32 +44,57 @@ public class DeveloperDetailFragment extends Fragment {
     public DeveloperDetailFragment() {
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        if (getArguments().containsKey(ARG_ITEM)) {
-            // Load the dummy content specified by the fragment
-            // arguments. In a real-world scenario, use a Loader
-            // to load content from a content provider.
-            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM));
-
-            Activity activity = this.getActivity();
-            CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
-            if (appBarLayout != null) {
-                appBarLayout.setTitle(mItem.content);
-            }
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.item_detail, container, false);
 
-        // Show the dummy content as text in a TextView.
-        if (mItem != null) {
-            ((TextView) rootView.findViewById(R.id.item_detail)).setText(mItem.details);
+
+        if (getArguments().containsKey(ARG_ITEM)) {
+            // Load the dummy content specified by the fragment
+            // arguments. In a real-world scenario, use a Loader
+            // to load content from a content provider.
+            mDeveloper = getArguments().getParcelable(ARG_ITEM);
+
+            Activity activity = this.getActivity();
+            CollapsingToolbarLayout collapsingToolbarLayout =
+                    (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
+
+            if (collapsingToolbarLayout != null) {
+                collapsingToolbarLayout.setTitle(getActivity().getTitle());
+
+                mImageView = (ImageView) collapsingToolbarLayout.findViewById(R.id.profile_image);
+
+            }
+            else {
+                mImageView = (ImageView) rootView.findViewById(R.id.profile_image_two_pane);
+                mImageView.setVisibility(View.VISIBLE);
+            }
+
+            Glide.with(getActivity())
+                    .load(mDeveloper.getImageUrl())
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .placeholder(R.drawable.ic_person_outline_black_32dp)
+                    .into(mImageView);
+
+
+        }
+
+        if (mDeveloper != null) {
+            ((TextView) rootView.findViewById(R.id.username)).setText(mDeveloper.getUsername());
+           TextView githubLinkTextView = ((TextView) rootView.findViewById(R.id.github_link));
+
+            githubLinkTextView.setText(mDeveloper.getGithubProfileUrl());
+            githubLinkTextView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse(mDeveloper.getGithubProfileUrl()));
+                    startActivity(intent);
+                }
+            });
+
         }
 
         return rootView;
